0777ef510301c50a314d1ce6d75b18e0b65934a5
Alona A <alyaakhiezer@gmail.com>
2019-11-27 11:54:36 +0200

    modified:   angular.json modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	deleted:    src/app/app.component.spec.ts modified:   src/app/app.component.ts modified:   src/app/app.module.ts deleted:    src/app/places-filter.pipe.spec.ts 	modified:   src/index.html 	modified:   src/main.ts modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Output, OnInit } from '@angular/core';
+import { IPlace, places, IWeather, ISocial } from 'src/mock/itemsData';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'placesHW';
+
+export class AppComponent implements OnInit {
+  title = 'PlacesHW';
+
+  public places: IPlace[] = places;
+
+  @Output()
+  public weather: IWeather;
+
+  @Output()
+  public social: ISocial;
+
+  public currentPlace: IPlace;
+
+  public placesTypes: string[];
+
+  public selectedType:string;
+
+  selectPlace(place: IPlace): void {
+    this.weather = place.weather; 
+    this.social = place.social_info;
+    this.currentPlace = place;
+  }
+
+  public ngOnInit(){    
+    this.initPlacesTypes();
+    this.selectPlace(places[0]);
+  }
+
+  initPlacesTypes(): void{
+    this.placesTypes = places.map(x => x.type);
+    this.placesTypes = this.placesTypes.filter((v,i) => this.placesTypes.indexOf(v) === i);
+    this.selectedType = this.placesTypes[0];
+  }
+
+  selectType(type: string): void{
+    this.selectedType = type;
+    this.selectPlace(this.places.filter((x) => x.type == type)[0]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaceComponent } from './place/place.component';
+import { WeatherComponent } from './weather/weather.component';
+import { SocialComponent } from './social/social.component';
+import { PhonePipe } from './phone.pipe';
+import { PlacesFilterPipe } from './places-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaceComponent,
+    WeatherComponent,
+    SocialComponent,
+    PhonePipe,
+    PlacesFilterPipe    
   ],
   imports: [
     BrowserModule,
